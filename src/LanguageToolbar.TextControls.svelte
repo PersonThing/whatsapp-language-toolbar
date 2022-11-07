bf2c6c6abb0ed399e033fb39ee4c3bfc7daeb87e
bruno34154 <www.brunomesenga@gmail.com>
2022-11-07 10:44:01 -0300

    adding more languages and changing the menu to select instead buttons

--- src/LanguageToolbar.TextControls.svelte
+++ src/LanguageToolbar.TextControls.svelte
@@ -1,27 +1,52 @@
-{#each langs as lang}
-  <button on:click={() => translateText(lang.short)}>{lang.long} 🠆</button>
-{/each}
+<select id="select"  on:change={()=>translateText()} >
+  <option disabled selected>Select a language to translate</option>
+  {#each langs as lang} 
+    <option class="option" value={lang.short}>{lang.long} 🠆</option>
+  {/each}
+</select>
+
 
 <script>
   import languages from './languages.js'
-
+  
   export let text = null
   
   let langs = [
    languages.pt,
    languages.es,
-   languages.en
+   languages.en,
+   languages.ja,
+   languages.fr,
+   languages.it,
+   languages.de,
+   languages.ko,
+   
+   
   ]
 
-  function translateText(targetLang) {
+  function translateText() {
+    let select =  document.getElementById("select");
+    let targetLang = select.value;
     if (!text?.length) return
     const translationUrl = `https://translate.google.com/#view=home&op=translate&sl=auto&tl=${targetLang}&text=${encodeURIComponent(text)}`
     window.open(translationUrl, '_blank')
   }
 </script>
-
+ 
 <style>
-  button {
+  select{
+    padding: 5px 6px;
+    background-color: rgb(32,44,51);
+    border: none;
+    color: white;
+    font-size: 12px;
+    font-family: Arial;
+    border-radius: 4px;
+    width: 100%;
+    cursor: pointer;
+  }
+  
+  /*button {
     padding: 5px 6px;
     background: none;
     border: none;
@@ -35,5 +60,5 @@
 
   button:hover {
     background: rgba(255, 255, 255, 0.1);
-  }
+  }*/
 </style>
